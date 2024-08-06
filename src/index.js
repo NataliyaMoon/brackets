3fc16f04c9f4bfa08e15a6f8dcbdf0211c6c6a86
Moon Natalia <nataliya.moon@gmail.com>
2024-08-06 20:03:20 +0500

    feat: add solution to the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const bracketMap = new Map();
+  const stack = [];
+  
+  bracketsConfig.forEach(([open, close]) => {
+    bracketMap.set(open, close);
+  });
+  
+
+  for (let char of str) {
+    if (bracketMap.has(char)) {
+     
+      if (bracketMap.get(char) === char && stack.length > 0 && stack[stack.length - 1] === char) {
+       
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else {
+     
+      if (stack.length === 0 || bracketMap.get(stack.pop()) !== char) {
+        return false;
+      }
+    }
+  }
+  
+  return stack.length === 0;
 }
